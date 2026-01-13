f551e6fa525dd2f16fddfb0b06e5b660623fb622
samuhaug <samu.haug@gmail.com>
2026-01-13 13:32:48 +0000

    Initial commit for Python Praxis project

--- test.py
+++ test.py
@@ -1,3 +1,6 @@
-input("geben sie Temperatur ein")
+Temperatur = int(input("Geben Sie die Temperatur ein!"))
+zu_kalt = (Temperatur < 20)
+zu_warm = (Temperatur > 80)
+ist_in_ordnung = not(zu_kalt or zu_warm)
 
-
+print("Die Temperatur ist in Ordnung:", zu_kalt, zu_warm, ist_in_ordnung)
