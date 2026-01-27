9549f9c1d11cef0fe608852c8cb84b82479a59cc
samuhaug <samu.haug@gmail.com>
2026-01-27 17:06:45 +0000

    1

---
+++ cnc_drehzahl_sequenzen.py
@@ -0,0 +1,13 @@
+print("CNC Drehzahl Sequenzen")
+for drehen_drehzahl in range (200,2001,200):
+    print (drehen_drehzahl, end=" ")
+print() #Zeilenumbruch
+for fräsen_drehzahl in range(3000,10000,1000):
+    print(fräsen_drehzahl, end=" ")
+
+for bohren_drehzahl in range(1000,5000,500):
+    print(bohren_drehzahl, end=" ")
+for hochlauf_test in range(0,1000,100):
+    print(hochlauf_test, end=" ")
+for notfall_abbruch in range(3000, -1, -500):
+    print(notfall_abbruch, end=" ")
