293f4aac96e905f87b51e2f54bac5a93eb523b5f
samuhaug <samu.haug@gmail.com>
2026-02-03 16:11:57 +0000

    1# Please enter the commit message for your changes. Lines starting

--- cnc_drehzahl_sequenzen.py
+++ cnc_drehzahl_sequenzen.py
@@ -4,10 +4,12 @@ for drehen_drehzahl in range (200,2001,200):
 print() #Zeilenumbruch
 for fräsen_drehzahl in range(3000,10000,1000):
     print(fräsen_drehzahl, end=" ")
-
+print() #Zeilenumbruch
 for bohren_drehzahl in range(1000,5000,500):
     print(bohren_drehzahl, end=" ")
+print() #Zeilenumbr
 for hochlauf_test in range(0,1000,100):
     print(hochlauf_test, end=" ")
+print() #Zeilenumbr
 for notfall_abbruch in range(3000, -1, -500):
     print(notfall_abbruch, end=" ")
